2b05624a87b76d9b86f8381caa7ab6e02b52ff40
Maria Luna <mlunat20@gmail.com>
2025-08-03 22:24:28 -0500

    implement user repository

---
+++ src/infraestructure/persistence/userRepositoryPostgres.ts
@@ -0,0 +1,45 @@
+import type { Knex } from "knex";
+import { Database } from "../database/db";
+
+import { IUserRepository } from "../../application/ports/userRepository";
+import { User } from "../../domain/entities/user";
+
+export class UserRepositoryPostgres implements IUserRepository {
+  trx: Knex | Knex.Transaction;
+
+  constructor(trx?: Knex | Knex.Transaction) {
+    this.trx = trx ?? Database.instance();
+  }
+
+  async create(user: User): Promise<User> {
+    //TODO: implement mapper
+    const userToDB = {
+      id: user.id,
+      first_name: user.firstName,
+      last_name: user.lastName,
+      birthdate: user.birthdate,
+      created_at: user.createdAt,
+      updated_at: user.updatedAt,
+      deleted_at: user.deletedAt,
+      email: user.email,
+      country_id: user.countryId,
+    };
+
+    await this.trx<User>("userstest").insert(userToDB);
+    return user;
+  }
+
+  // TODO: pending implement
+  findByEmail(email: string): Promise<User | undefined> {
+    return this.trx<User>("userstest").where("email", email).first();
+  }
+  update(user: User): Promise<User> {
+    console.log(user);
+    throw new Error("Method update not implemented.");
+  }
+  delete(id: string): Promise<void> {
+    // this will be a soft deleted
+    console.log(id);
+    throw new Error("Method delete not implemented.");
+  }
+}
