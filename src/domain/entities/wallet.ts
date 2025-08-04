bcfa0bd8687742181bd26585e0da80693574f4b2
Maria Luna <mlunat20@gmail.com>
2025-08-04 17:26:03 -0500

    add login of domain for credit and debit

--- src/domain/entities/wallet.ts
+++ src/domain/entities/wallet.ts
@@ -1,7 +1,7 @@
 export interface IWallet {
   id: string;
   userId: string;
-  amount: number;
+  total: number;
   status: boolean;
   monthlyRecurrence: number;
   createdAt: Date;
@@ -12,7 +12,7 @@ export interface IWallet {
 export class Wallet implements IWallet {
   id: string;
   userId: string;
-  amount: number;
+  total: number;
   status: boolean;
   monthlyRecurrence: number;
   createdAt: Date;
@@ -22,11 +22,31 @@ export class Wallet implements IWallet {
   constructor(wallet: IWallet) {
     this.id = wallet.id;
     this.userId = wallet.userId;
-    this.amount = wallet.amount;
+    this.total = wallet.total;
     this.status = wallet.status;
     this.monthlyRecurrence = wallet.monthlyRecurrence;
     this.createdAt = wallet.createdAt;
     this.updatedAt = wallet.updatedAt;
     this.deletedAt = wallet.deletedAt;
+  }
+
+  public debit(amount: number): void {
+    if (amount <= 0) throw new Error(`Invalid amount: ${amount}`);
+
+    if (this.total < amount) {
+      throw new Error(`Insufficient funds - currency funds: ${this.total}`);
+    }
+
+    this.total -= amount;
+  }
+
+  public credit(amount: number): void {
+    if (amount <= 0) throw new Error(`Invalid amount: ${amount}`);
+
+    this.total += amount;
+  }
+
+  public getTotal(): number {
+    return this.total;
   }
 }
